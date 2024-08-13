fc171e4446cb0e3728655955b1206f785072d0f0
Administrator <Administrator@163.com>
2024-08-13 09:24:44 +0800

    优化文件管理

--- src/main/java/com/so/component/util/ConfirmationDialogPopupWindow.java
+++ src/main/java/com/so/component/util/ConfirmationDialogPopupWindow.java
@@ -17,103 +17,120 @@ import com.vaadin.ui.VerticalLayout;
  */
 public class ConfirmationDialogPopupWindow extends PopupWindow {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected VerticalLayout layout;
-	protected Label descriptionLabel;
-	protected Button yesButton;
-	protected Button noButton;
+    protected VerticalLayout layout;
+    protected Label descriptionLabel;
+    protected Button yesButton;
+    protected Button noButton;
 
-	public ConfirmationDialogPopupWindow(String title, String description, String sureCaption, String cancleCaption,
-			boolean isClosable) {
-		setWidth("429px");
-		setHeight("190px");
-		setModal(true);
-		setResizable(true);
-		this.setClosable(isClosable);
+    public ConfirmationDialogPopupWindow(String title, String description, String sureCaption, String cancleCaption,
+                                         boolean isClosable) {
+        setWidth("429px");
+        setHeight("190px");
+        setModal(true);
+        setResizable(true);
+        this.setClosable(isClosable);
 
-		layout = new VerticalLayout();
-		layout.setMargin(true);
-		layout.setSpacing(true);
-		layout.setSizeFull();
-		layout.addStyleName("common-popWindow-style");
+        layout = new VerticalLayout();
+        layout.setMargin(true);
+        layout.setSpacing(true);
+        layout.setSizeFull();
+        layout.addStyleName("common-popWindow-style");
 
-		setContent(layout);
+        setContent(layout);
 
-		if (title != null) {
-			setCaption(title);
-		} else {
-			setCaption("提示");
-		}
+        if (title != null) {
+            setCaption(title);
+        } else {
+            setCaption("提示");
+        }
 
-		initLabel(description);
-		initButtons(sureCaption, cancleCaption);
-	}
+        initLabel(description);
+        initButtons(sureCaption, cancleCaption);
+    }
 
-	public ConfirmationDialogPopupWindow(String description, String sureCaption, String cancleCaption) {
-		this(null, description, sureCaption, cancleCaption, true);
-	}
+    public ConfirmationDialogPopupWindow(String description, String sureCaption, String cancleCaption) {
+        this(null, description, sureCaption, cancleCaption, true);
+    }
 
-	/**
-	 * Show the confirmation popup.
-	 */
-	public void showConfirmation() {
+    /**
+     * Show the confirmation popup.
+     */
+    public void showConfirmation() {
 //		yesButton.focus();
-		UI.getCurrent().addWindow(this);
-	}
+        UI.getCurrent().addWindow(this);
+    }
 
-	protected void initButtons(String sureCaption, String cancleCaption) {
-		if (sureCaption != null) {
-			yesButton = new Button(sureCaption);
-		} else {
-			yesButton = new Button("保存");
-		}
-		yesButton.addClickListener(new ClickListener() {
-			private static final long serialVersionUID = 1L;
+    protected void initButtons(String sureCaption, String cancleCaption) {
+        if (sureCaption != null) {
+            yesButton = new Button(sureCaption);
+        } else {
+            yesButton = new Button("保存");
+        }
+//        yesButton.addClickListener(new ClickListener() {
+//            private static final long serialVersionUID = 1L;
+//
+//            @Override
+//            public void buttonClick(ClickEvent event) {
+//                close();
+//                fireEvent(new ConfirmationEvent(ConfirmationDialogPopupWindow.this, true));
+//            }
+//        });
 
-			public void buttonClick(ClickEvent event) {
-				close();
-				fireEvent(new ConfirmationEvent(ConfirmationDialogPopupWindow.this, true));
-			}
-		});
+        if (cancleCaption != null) {
+            noButton = new Button(cancleCaption);
+        } else {
+            noButton = new Button("不保存");
+        }
+        noButton.addClickListener(new ClickListener() {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public void buttonClick(ClickEvent event) {
+                close();
+                fireEvent(new ConfirmationEvent(ConfirmationDialogPopupWindow.this, false));
+            }
+        });
+        Label blankLabelOther = new Label();
+        blankLabelOther.setWidth("10px");
 
-		if (cancleCaption != null) {
-			noButton = new Button(cancleCaption);
-		} else {
-			noButton = new Button("不保存");
-		}
-		noButton.addClickListener(new ClickListener() {
-			private static final long serialVersionUID = 1L;
+        Label blankLabel = new Label();
+        blankLabel.setWidth("10px");
 
-			public void buttonClick(ClickEvent event) {
-				close();
-				fireEvent(new ConfirmationEvent(ConfirmationDialogPopupWindow.this, false));
-			}
-		});
-		Label blankLabelOther = new Label();
-		blankLabelOther.setWidth("10px");
+        HorizontalLayout buttonLayout = new HorizontalLayout(blankLabel, yesButton, blankLabelOther, noButton);
+        buttonLayout.setWidth("100%");
+        buttonLayout.setComponentAlignment(yesButton, Alignment.BOTTOM_RIGHT);
+        buttonLayout.setComponentAlignment(noButton, Alignment.BOTTOM_RIGHT);
+        buttonLayout.setExpandRatio(blankLabel, 1.0f);
 
-		Label blankLabel = new Label();
-		blankLabel.setWidth("10px");
+        layout.addComponent(buttonLayout);
+    }
 
-		HorizontalLayout buttonLayout = new HorizontalLayout(blankLabel, yesButton, blankLabelOther, noButton);
-		buttonLayout.setWidth("100%");
-		buttonLayout.setComponentAlignment(yesButton, Alignment.BOTTOM_RIGHT);
-		buttonLayout.setComponentAlignment(noButton, Alignment.BOTTOM_RIGHT);
-		buttonLayout.setExpandRatio(blankLabel, 1.0f);
+    protected void initLabel(String description) {
+        descriptionLabel = new Label(description, ContentMode.HTML);
+        descriptionLabel.addStyleName("common-popWindow-content");
+        descriptionLabel.setWidth("390px");
 
-		layout.addComponent(buttonLayout);
-	}
+        Label iconLabel = new Label();
+        iconLabel.setIcon(new ThemeResource("img/sure.png"));
 
-	protected void initLabel(String description) {
-		descriptionLabel = new Label(description,ContentMode.HTML);
-		descriptionLabel.addStyleName("common-popWindow-content");
-		descriptionLabel.setWidth("390px");
+        HorizontalLayout msgLayout = new HorizontalLayout(iconLabel, descriptionLabel);
+        layout.addComponent(msgLayout);
+    }
 
-		Label iconLabel = new Label();
-		iconLabel.setIcon(new ThemeResource("img/sure.png"));
+    public Button getYesButton() {
+        return yesButton;
+    }
 
-		HorizontalLayout msgLayout = new HorizontalLayout(iconLabel, descriptionLabel);
-		layout.addComponent(msgLayout);
-	}
+    public void setYesButton(Button yesButton) {
+        this.yesButton = yesButton;
+    }
+
+    public Button getNoButton() {
+        return noButton;
+    }
+
+    public void setNoButton(Button noButton) {
+        this.noButton = noButton;
+    }
 }
